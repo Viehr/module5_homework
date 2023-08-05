8808400392ac7257c8386ed7f6b343c2e7716a7b
Alexandra <sasanori32195@gmail.com>
2023-08-05 13:25:51 +0300

    2и3 тренажер

--- training.js
+++ training.js
@@ -12,14 +12,14 @@ alert(`${cels} градусов по Цельсию равны ${far} граду
 
 
 
-//задание 1.4
+//задание 1.3
 let x = prompt('');
 let y = (4*x*x + 18 - 23*x)/(5*x/9 + 18*x*x*x*x/33) + x*15/12;
 alert(y);
 
 
 
-//задание 1.5
+//задание 1.4
 a = 12;
 b = 'number';
 c = false;
@@ -46,7 +46,7 @@ alert(at && bt && ct && dt && et && ft && gt && ht && it && jt);
 
 
 
-//задание 2.1
+//задание 1.5
 const NDS = 0.20
 const NDFL = 0.13
 const EXCISE = 0.1
@@ -68,24 +68,92 @@ alert('Осталось ' + totalAvailable + ' рублей')
 
 
 
+
+
+
+
+//задание 2.1
+const katA = prompt('');
+const katB = prompt('');
+const gipC = prompt('');
+
+if (katA**2 + katB**2 === gipC) {
+  alert('Это прямоугольный треугольник!')
+} else {
+  alert('Это не прямоугольный треугольник!')
+};
+/*мой код почему-то не работает, вот код ответ на тренажер:
+const a = prompt('Введите катет А')
+const b = prompt('Введите катет B')
+const c = prompt('Введите гипотенузу C')
+
+if (a ** 2 + b ** 2 === c ** 2) {
+	alert('Это прямоугольный треугольник!')
+} else {
+	alert('Это не прямоугольный треугольник!')
+}*/
+
+
+
+
 //задание 2.2
+const a = prompt('');
+const b = prompt('');
+const c = prompt('');
+
+if (a>b) {
+  alert (a);
+} else {
+  if (b>c) {
+    alert(b);
+  } else {
+    alert(c)
+  }
+};
 
 
 
 
 //задание 2.3
-
+const number = prompt('');
+if (number%2 === 0) {
+  alert(`Число ${number} четное!`)
+} else {
+  alert(`Число ${number} нечетное!`)
+};
 
 
 
 
 //задание 3.1
-
+for (let i = 1; i <= 11; i += 2) {
+	if (i > 5) {
+	  console.log(i - 1);
+  } else {
+    console.log(i)
+  }
+}
 
 
 
 
 //задание 3.2
+let sum = 0;
+let trip;
+while (trip != 0) {
+  trip = prompt('');
+  sum += +trip;
+};
+console.log(sum);
+
+/* мой код почеиу-то не работает, вот ответ на этот тренажер:
+let sum = 0;
+let a;
+do {
+  a = prompt('Введите число для суммирования. 0 - остановить');
+  sum += +a;
+} while (a != 0);
+alert(sum);*/
 
 
 
